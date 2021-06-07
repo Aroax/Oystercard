9d8aa0d4d6a911874256e9bc2021acf4c8374d5b
Arran Oakes <arran.oakes@gmail.com>
2021-06-07 17:52:35 +0100

    refactored unit tests after touch_in min fare

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,26 +9,6 @@ let(:min_fare) { Oystercard::MINIMUM_FARE }
     expect(subject.balance).to eq(0)
   end
 
-  describe '#top_up' do
-    it 'can top up' do
-      top_up_amount = 50
-      card.top_up(top_up_amount)
-      expect(card.balance).to eq(50)
-    end
-
-    it 'raises an error if top_up would take balance over 90' do
-      top_up_amount = max_limit + 1
-      expect{ card.top_up(top_up_amount) }.to raise_error "Cannot top up: balance would be above #{max_limit} maximum"
-    end
-  end
-
-  it 'deducts a fare from the balance' do
-    fare = 10
-    top_up_amount = 20
-    card.top_up(top_up_amount)
-    expect{card.deduct(fare)}.to change{card.balance}.from(top_up_amount).to(top_up_amount - fare)
-  end
-
   it 'can track whether it is on a journey or not' do
     expect(card).to respond_to(:in_journey?)
   end
@@ -40,6 +20,19 @@ let(:min_fare) { Oystercard::MINIMUM_FARE }
       expect(card.in_journey?).to be(true)
     end
 
+    describe '#top_up' do
+      it 'can top up' do
+        top_up_amount = 50
+        card.top_up(top_up_amount)
+        expect(card.balance).to eq(50)
+      end
+
+      it 'raises an error if top_up would take balance over 90' do
+        top_up_amount = max_limit + 1
+        expect{ card.top_up(top_up_amount) }.to raise_error "Cannot top up: balance would be above #{max_limit} maximum"
+      end
+    end
+
     it 'raises an error if try to touch in without the minimum fare' do
       expect{card.touch_in}.to raise_error "Cannot touch in: not enough funds"
     end
@@ -48,6 +41,13 @@ let(:min_fare) { Oystercard::MINIMUM_FARE }
   context 'when on a journey' do
     it 'can be touched out' do
       expect(card.touch_out).to eq(false)
+    end
+
+    it 'deducts a fare from the balance' do
+      fare = 10
+      top_up_amount = 20
+      card.top_up(top_up_amount)
+      expect{card.deduct(fare)}.to change{card.balance}.from(top_up_amount).to(top_up_amount - fare)
     end
   end
 end
