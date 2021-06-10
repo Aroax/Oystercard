8b0be404ce7358ca011120591ba78429ad7915e0
Arran Oakes <arran.oakes@gmail.com>
2021-06-10 16:49:37 +0100

    added fare calculation and penalty to Journey object

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,9 +1,15 @@
+require_relative 'oystercard'
+
 class Journey
 attr_reader :on_journey, :entry_station, :exit_station
+MINIMUM_FARE = Oystercard::MINIMUM_FARE
+PENALTY_FARE = 6
 
-  def initialize(entry_station)
+  def initialize
     @on_journey = true
-    @entry_station = entry_station
+    @entry_station = ""
+    @exit_station = ""
+    @zone_fares = {}
   end
 
   def top_up(value)
@@ -12,17 +18,29 @@ attr_reader :on_journey, :entry_station, :exit_station
   end
 
   def complete?
-    
+
   end
 
-  def set_exit(new_exit_station)
-    @exit_station = new_exit_station
+  def set_entry(entry_station)
+    @entry_station = entry_station
+  end
+
+  def set_exit(exit_station)
+    @exit_station = exit_station
+  end
+
+  def calculate_fare
+    if (@entry_station == "" || @exit_station == "")
+      PENALTY_FARE
+    else
+      MINIMUM_FARE
+    end
   end
 
   private
 
-  def busted?(top_up)
-    @balance + top_up > MAX_LIMIT
-  end
-  
+  # def busted?(top_up)
+  #   @balance + top_up > MAX_LIMIT
+  # end
+
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -5,6 +5,7 @@ describe Journey do
   let(:journey) { Journey.new }
   let(:max_limit) { Oystercard::MAX_LIMIT }
   let(:min_fare) { Oystercard::MINIMUM_FARE }
+  let(:penalty_fare) { Journey::PENALTY_FARE }
   let(:entry_station) { double :entry_station }
   let(:exit_station) { double :exit_station }
   let(:token_top_up) { 20 }
@@ -12,26 +13,44 @@ describe Journey do
 
 
   it 'knows whether a journey is complete' do
-    journey = Journey.new(entry_station)
+    # journey = Journey.new(entry_station)
     expect(journey).to respond_to(:complete?)
   end
 
   it "knows when it is on a journey" do
-    journey = Journey.new(entry_station)
+    # journey = Journey.new(entry_station)
     expect(journey.on_journey).to eq(true || false)
   end
 
   context "when making a journey" do
     it 'stores the entry station' do
-      journey = Journey.new(entry_station)
+      # journey = Journey.new(entry_station)
+      journey.set_entry(entry_station)
       expect(journey.entry_station).to eq(entry_station)
     end
 
     it 'store the exit station' do
-      journey = Journey.new(entry_station)
-      journey.set_exit("Whitechapel")
-      expect(journey.exit_station).to eq("Whitechapel")
+      # journey = Journey.new(entry_station)
+      journey.set_exit(exit_station)
+      expect(journey.exit_station).to eq(exit_station)
     end
+
+    describe '#calculate_fare' do
+      it 'calculates the fare of the journey' do
+        # journey = Journey.new("Victoria")
+        journey.set_entry(entry_station)
+        journey.set_exit(exit_station)
+        expect(journey.calculate_fare).to be_an_instance_of(Integer)
+      end
+
+      it "returns penalty fare if there is neither entry nor exit station" do
+        # journey = Journey.new("Victoria")
+        journey.set_entry(entry_station)
+        expect(journey.calculate_fare).to eq(penalty_fare)
+      end
+    end
+
+
   end
 
 
